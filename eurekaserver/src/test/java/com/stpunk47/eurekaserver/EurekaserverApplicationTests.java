bcc03baa7bd686e2e038ab322faa7113170d5f07
@stpunk47 <stpunk47@gmail.com>
2025-04-30 21:35:03 +0300

    Setup Service Discovery agent using Eureka Server.

---
+++ eurekaserver/src/test/java/com/stpunk47/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

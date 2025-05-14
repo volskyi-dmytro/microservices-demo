b88ef5c9e97cf9772abaeed251b5cc6901534522
@stpunk47 <stpunk47@gmail.com>
2025-05-14 20:11:50 +0300

    Implement custom routing using Spring Cloud Gateway.

--- gatewayserver/src/main/java/com/stpunk47/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/stpunk47/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,31 @@ package com.stpunk47.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator stBankRouteConfig(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+				.route(r -> r.path("/stbank/accounts/**")
+						.filters(f -> f.rewritePath("/stbank/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(r -> r.path("/stbank/cards/**")
+						.filters(f -> f.rewritePath("/stbank/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.route(r -> r.path("/stbank/loans/**")
+						.filters(f -> f.rewritePath("/stbank/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.build();
 	}
 
 }

9eb501aba5bb5d046563e0ec791bdfad32cc00de
@stpunk47 <stpunk47@gmail.com>
2025-05-22 01:27:01 +0300

    Implement Redis RateLimiter in Gateway.

--- gatewayserver/src/main/java/com/stpunk47/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/stpunk47/gatewayserver/GatewayserverApplication.java
@@ -2,10 +2,13 @@ package com.stpunk47.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -43,9 +46,27 @@ public class GatewayserverApplication {
 										.setFallbackUri("forward:/contactSupport"))
 								.retry(retryConfig -> retryConfig.setRetries(3)
 										.setMethods(HttpMethod.GET)
-										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+								.requestRateLimiter(config -> config
+										.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
 						.uri("lb://LOANS"))
 				.build();
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1,1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono
+				.justOrEmpty(exchange
+						.getRequest()
+						.getHeaders()
+						.getFirst("user"))
+				.defaultIfEmpty("anonymous");
 	}
 
 }

254ccc9775e8d6d5ddf6782a0300a740b7049387
@stpunk47 <stpunk47@gmail.com>
2025-05-21 21:26:44 +0300

    Implement Circuit Breaker with Feign Client.

---
+++ accounts/src/main/java/com/stpunk47/accounts/service/client/LoansFallback.java
@@ -0,0 +1,13 @@
+package com.stpunk47.accounts.service.client;
+
+import com.stpunk47.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient {
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
--- accounts/src/main/java/com/stpunk47/accounts/service/client/LoansFeignClient.java
+++ accounts/src/main/java/com/stpunk47/accounts/service/client/LoansFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("loans")
+@FeignClient(value = "loans", fallback = LoansFallback.class)
 public interface LoansFeignClient {
 
     @GetMapping(value = "/api/fetch", consumes = "application/json")

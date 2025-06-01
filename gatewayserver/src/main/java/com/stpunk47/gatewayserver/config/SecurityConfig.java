a53dc1b2083170068b0eff33598fd754c87a2787
@stpunk47 <stpunk47@gmail.com>
2025-06-01 19:27:06 +0300

    Implement Authorization inside Gateway server using Roles.

--- gatewayserver/src/main/java/com/stpunk47/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/stpunk47/gatewayserver/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.stpunk47.gatewayserver.config;
 
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,15 +24,20 @@ public class SecurityConfig {
         http.authorizeExchange(
                 exchanges -> exchanges
                         .pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/stbank/accounts/**").authenticated()
-                        .pathMatchers("/stbank/cards/**").authenticated()
-                        .pathMatchers("/stbank/loans/**").authenticated())
+                        .pathMatchers("/stbank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/stbank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/stbank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(
                         oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                                .jwt(Customizer.withDefaults()));
+                                .jwt(jwtSpec ->
+                                        jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return http.build();
     }
 
-
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
 }
